bc097ddc077f543a4fc1bbd13d85ece4e454285f
KamaruzzamanFahad <mihacker41@gmail.com>
2024-04-27 22:54:51 +0600

    data delete method bug fix

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middle ware
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://modern-jute-wooden.web.app"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  withCredentials: true,
+}))
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.otpbube.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,53 +28,54 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+ 
     const database = client.db('ArtAndCraft')
     const craftcollection = database.collection('Craft')
     const subcatagorycollection = database.collection('subcategory_Name')
 
-    app.get('/craft', async (req,res) =>{
-        const data = craftcollection.find()
-        const result = await data.toArray()
-        res.send(result);
+    app.get('/craft', async (req, res) => {
+      const data = craftcollection.find()
+      const result = await data.toArray()
+      res.send(result);
     })
 
-    app.get('/subcatagory', async (req,res) =>{
-        const data = subcatagorycollection.find()
-        const result = await data.toArray()
-        res.send(result);
+    app.get('/subcatagory', async (req, res) => {
+      const data = subcatagorycollection.find()
+      const result = await data.toArray()
+      res.send(result);
     })
 
-    app.get('/craft/:id',(req,res)=>{
+
+    app.get('/product/:email', async (req, res) => {
+      const email = req.params.email;
+      const quary = { email: email }
+      const result = await craftcollection.find(quary).toArray()
+      res.send(result);
+      console.log(email);
+    })
+
+    app.get('/craft/:id', async (req, res) => {
       const id = req.params.id;
-      console.log(id)
-    })
-
-    app.get('/craft/:id', async(req,res)=> {
-        const email = req.params.id;
-        const quary = {email: email}
-        const result = await craftcollection.find(quary).toArray()
-        res.send(result);
-        console.log(email);
+      const quary = {_id: new ObjectId(id)}
+      const result = await craftcollection.find(quary).toArray()
+      res.send(result);
+      console.log(id);
     })
 
 
-    app.post('/craft', async (req,res) => {
-        const doc = req.body;
-        const result = await craftcollection.insertOne(doc)
-        res.send(result);
+    app.post('/craft', async (req, res) => {
+      const doc = req.body;
+      const result = await craftcollection.insertOne(doc)
+      res.send(result);
     })
 
-    
-
-
-
-
-
-
-
-
-
+    app.delete('/delete/:id', async(req,res)=>{
+      const id = req.params.id;
+      console.log('heat delet',id)
+      const quary = {_id: new ObjectId(id)}
+      const result = await craftcollection.deleteOne(quary);
+      res.send(result)
+    })
 
 
 
@@ -78,7 +83,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+   
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -92,11 +97,11 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req,res) =>{
-    console.log('server is running port: ',port)
-    res.send(`server is running post: ${port} `)
+app.get('/', (req, res) => {
+  console.log('server is running port: ', port)
+  res.send(`server is running post: ${port} `)
 })
 
 app.listen(port, () => {
-    console.log('server running port: ',port)
+  console.log('server running port: ', port)
 })
